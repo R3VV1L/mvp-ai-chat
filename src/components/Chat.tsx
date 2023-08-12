3d4387d4f18a7e2326edfdbc1ccac509b2fa614b
R3VV1L <r3vv1l@mail.ru>
2023-08-12 10:32:29 +0300

    fix style chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react';
 import './Chat.css';
+import { useEffect, useRef, useState } from 'react';
 
 interface ChatProps {
     messages: string[];
-    onSendMessage: (message: string) => void;
+    onSendMessage: (message: string, time: string) => void;
 }
+
 export const Chat: React.FC<ChatProps> = ({ messages, onSendMessage }) => {
     const [message, setMessage] = useState('');
+    const [messageTimes, setMessageTimes] = useState<string[]>([]);
+    const messagesRef = useRef<HTMLDivElement>(null);
 
     const handleSendMessage = () => {
-        onSendMessage(message);
+        if (message.trim() === '') {
+            return;
+        }
+
+        const currentTime = getCurrentTime();
+        onSendMessage(message, currentTime);
+
+        const url = 'http://sharon.drach.pro:8081/question';
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ q: message }),
+        };
+        console.log({ q: message });
+        fetch(url, requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+
         setMessage('');
+        setMessageTimes([...messageTimes, currentTime]);
     };
+
     const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
             handleSendMessage();
         }
     };
+
     const getCurrentTime = () => {
         const currentTime = new Date();
         const hours = currentTime.getHours();
-        const minutes = currentTime.getMinutes().toString().padStart(2, '0'); // Добавляем лидирующий ноль, если минуты меньше 10
-        return `${hours}_${minutes}'`;
+        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
     };
+
+    useEffect(() => {
+        if (messagesRef.current) {
+            messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+        }
+    }, [messages]);
+
     return (
         <div>
-            <div className="messages">
+            <div className="messages" ref={messagesRef}>
                 {messages.map((msg, index) => (
                     <div key={index} className={`message message-right`}>
                         <span className="message-text">{msg}</span>
-                        <span className="message-time">{getCurrentTime()}</span>
+                        <span className="message-time">
+                            {messageTimes[index]}
+                        </span>
                     </div>
                 ))}
             </div>
